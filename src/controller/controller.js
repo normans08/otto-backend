477c9db9a54af11a998b95fb52c0f410e18579ab
devrt2786 <rt2786@gmail.com>
2023-04-05 13:50:53 +0530

    chnages

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,14 +1,25 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const transcribeGpt = async (req, res) => {
-  console.log("req", req.body);
   const configuration = new Configuration({
     apiKey: process.env.GPT_TOKEN,
   });
   try {
     const openai = new OpenAIApi(configuration);
-    const completion = await openai.createChatCompletion({
+    let options = {
       model: "gpt-3.5-turbo",
+      temperature: 0,
+      max_tokens: 1000,
+      top_p: 0.1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+      // stop: ["^"],
+    };
+
+    let completeOptions = {
+      ...options,
+      // prompt: `${newQuestion}
+      //  ${formatedText}`,
       messages: [
         {
           role: "user",
@@ -16,8 +27,8 @@ const transcribeGpt = async (req, res) => {
       ${req.body.second}`,
         },
       ],
-    });
-    console.log("result====>", completion.data.choices[0].message);
+    };
+    const completion = await openai.createChatCompletion(completeOptions);
 
     return completion.data.choices[0].message;
   } catch (error) {

f4d5f8492ed0ff57bf665abd69d08fb07bbd033e
devrt2786 <rt2786@gmail.com>
2023-04-06 15:59:14 +0530

    cloud upload

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const routes = require("./src/routers/router");
 const cors = require("cors");
 const app = express();
-
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 app.use(express.json());
 require("dotenv").config();
 app.use(express.urlencoded({ extended: false }));
@@ -27,6 +29,79 @@ app.use((req, res, next) => {
     return res.status(200).json({});
   }
   next();
+});
+
+app.post("/audio/upload", async (req, res) => {
+  // Get the file name and extension with multer
+  const storage = multer.diskStorage({
+    filename: (req, file, cb) => {
+      const fileExt = file.originalname.split(".").pop();
+      const filename = `${new Date().getTime()}.${fileExt}`;
+      cb(null, filename);
+    },
+  });
+
+  // Filter the file to validate if it meets the required audio extension
+  const fileFilter = (req, file, cb) => {
+    console.log("file", file);
+    if (
+      file.mimetype === "audio/mp3" ||
+      file.mimetype === "audio/mpeg" ||
+      file.mimetype === "audio/x-m4a" ||
+      file.mimetype === "audio/m4a"
+    ) {
+      cb(null, true);
+    } else {
+      cb(
+        {
+          message: "Unsupported File Format",
+        },
+        false
+      );
+    }
+  };
+
+  // Set the storage, file filter and file size with multer
+  const upload = multer({
+    storage,
+    limits: {
+      fieldNameSize: 200,
+      fileSize: 5 * 1024 * 1024,
+    },
+    fileFilter,
+  }).single("audio");
+
+  // upload to cloudinary
+  upload(req, res, (err) => {
+    if (err) {
+      return res.send(err);
+    }
+
+    // SEND FILE TO CLOUDINARY
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+    const { path } = req.file; // file becomes available in req at this point
+
+    const fName = req.file.originalname.split(".")[0];
+    cloudinary.uploader.upload(
+      path,
+      {
+        resource_type: "raw",
+        public_id: `AudioUploads/${fName}`,
+      },
+
+      // Send cloudinary response or catch error
+      (err, audio) => {
+        if (err) return res.send(err);
+
+        fs.unlinkSync(path);
+        res.send(audio);
+      }
+    );
+  });
 });
 
 // simple route
--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,4 +1,7 @@
 const { Configuration, OpenAIApi } = require("openai");
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
 const transcribeGpt = async (req, res) => {
   const configuration = new Configuration({
@@ -34,6 +37,81 @@ const transcribeGpt = async (req, res) => {
   }
 };
 
+const uploadAudio = async (req, res) => {
+  // Get the file name and extension with multer
+  const storage = multer.diskStorage({
+    filename: (req, file, cb) => {
+      const fileExt = file.originalname.split(".")?.pop();
+      const filename = `${new Date().getTime()}.${fileExt}`;
+      cb(null, filename);
+    },
+  });
+
+  // Filter the file to validate if it meets the required audio extension
+  const fileFilter = (req, file, cb) => {
+    if (
+      file.mimetype === "audio/mp3" ||
+      file.mimetype === "audio/wave" ||
+      file.mimetype === "audio/mpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(
+        {
+          message: "Unsupported File Format",
+        },
+        false
+      );
+    }
+  };
+
+  // Set the storage, file filter and file size with multer
+  const upload = multer({
+    storage,
+    limits: {
+      fieldNameSize: 2000000,
+      fileSize: 5 * 1024 * 1024 * 1024 * 1024,
+    },
+    fileFilter,
+  }).single("audio");
+
+  // upload to cloudinary
+  upload(req, res, (err) => {
+    console.log("reqqqqq======>", req, res);
+    if (err) {
+      return res.send(err);
+    }
+
+    // SEND FILE TO CLOUDINARY
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+
+    const { path } = req.file; // file becomes available in req at this point
+    console.log("paarr", path);
+    const fName = req.file.originalname.split(".")[0];
+    cloudinary.uploader.upload(
+      path,
+      {
+        resource_type: "raw",
+        public_id: `AudioUploads/${fName}`,
+      },
+
+      // Send cloudinary response or catch error
+      (err, audio) => {
+        if (err) return res.send(err);
+
+        fs.unlinkSync(path);
+        console.log("ofefe", audio);
+        res.send(audio);
+      }
+    );
+  });
+};
+
 module.exports = {
   transcribeGpt,
+  uploadAudio,
 };
